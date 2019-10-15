a5e3e4593b29d5ac4997d9f9a632a0d211201a4d
Damian220690 <Damian.nieweg@gmail.com>
2019-10-15 11:23:22 +0200

    adding registration and login form

---
+++ src/main/java/pl/dn/betting_app/BettingAppApplication.java
@@ -0,0 +1,13 @@
+package pl.dn.betting_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BettingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BettingAppApplication.class, args);
+    }
+
+}
